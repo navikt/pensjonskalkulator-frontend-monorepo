b3029be049facde26bd8b0f4afed4706d180043b
w1n1c0d3s <eshaswini.murali.krishna@nav.no>
2026-02-02 17:17:44 +0100

    Fix eslint config

---
+++ apps/ekstern/eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import tseslint from 'typescript-eslint'
+
+export default [
+  {
+    ignores: ['dist/**', 'dist-sanity/**', 'coverage/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'no-irregular-whitespace': ['error', { skipTemplates: true }],
+    },
+  },
+]
