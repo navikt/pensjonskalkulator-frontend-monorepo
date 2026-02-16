81db7b73c956344f0206285ab5f1d7939e7930f8
martinstorvoll <martin.storvoll@nav.no>
2026-02-16 12:17:03 +0100

    Fjerner uburukt hooks

--- apps/intern/src/components/Beregning/Beregning.tsx
+++ apps/intern/src/components/Beregning/Beregning.tsx
@@ -1,14 +1,10 @@
-import { useBeregningQuery } from '../../api/queries'
-import { getFnrFromUrl } from '../../utils'
-import { useBeregningContext } from '../BeregningContext'
-
 import styles from './Beregning.module.css'
 
 export const Beregning = () => {
-	const fnr = getFnrFromUrl()
-	const { committedParams } = useBeregningContext()
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const _beregning = useBeregningQuery(fnr, committedParams)
+	// const fnr = getFnrFromUrl()
+	// const { committedParams } = useBeregningContext()
+	 
+	// const _beregning = useBeregningQuery(fnr, committedParams)
 
 	return (
 		<div className={styles.beregning}>
