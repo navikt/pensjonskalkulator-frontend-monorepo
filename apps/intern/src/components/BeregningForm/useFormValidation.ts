64d30436d6eeb06a3d530cb5b385136e81fbb8a2
w1n1c0d3s <eshaswini.murali.krishna@nav.no>
2026-03-18 15:59:39 +0100

    Fix epsAntallUtenlandsOppholdAar validation check

--- apps/intern/src/components/BeregningForm/useFormValidation.ts
+++ apps/intern/src/components/BeregningForm/useFormValidation.ts
@@ -17,7 +17,7 @@ function validateEPSOpplysninger(
 	formData: BeregningFormData,
 	errors: ValidationErrors
 ) {
-	if (formData.epsAntallUtenlandsOppholdAar === null) {
+	if (!formData.epsAntallUtenlandsOppholdAar) {
 		errors.epsAntallUtenlandsOppholdAar =
 			'Fyll ut år bodd/jobbet i utlandet etter fylte 16 år.'
 	}
