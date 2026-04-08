1219c8a9570da811903a2dcbf7f2b302f0594592
martinstorvoll <martin.storvoll@nav.no>
2026-04-08 12:48:51 +0200

    Table gaps

--- apps/intern/src/components/Beregning/BeregningTableWithSum.tsx
+++ apps/intern/src/components/Beregning/BeregningTableWithSum.tsx
@@ -49,14 +49,10 @@ export const BeregningTableWithSum = ({
 			<Table.Header>
 				<Table.Row className={styles.headerRow}>
 					<Table.HeaderCell>
-						<Label style={{ whiteSpace: 'nowrap' }} size="small">
-							{title}
-						</Label>
+						<Label size="small">{title}</Label>
 					</Table.HeaderCell>
 					<Table.HeaderCell align="right">
-						<Label style={{ whiteSpace: 'nowrap' }} size="small">
-							{valueHeader}
-						</Label>
+						<Label size="small">{valueHeader}</Label>
 					</Table.HeaderCell>
 				</Table.Row>
 			</Table.Header>
