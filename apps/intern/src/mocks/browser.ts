010e6c8914146a225e02add9f1ffbc644d833fd3
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2026-04-14 10:44:01 +0200

    Refactor: Simplify worker export in browser mocks and create new browser.ts in mocks package

--- apps/intern/src/mocks/browser.ts
+++ apps/intern/src/mocks/browser.ts
@@ -1,4 +1,1 @@
-import { handlers } from '@pensjonskalkulator-frontend-monorepo/mocks/handlers'
-import { setupWorker } from 'msw/browser'
-
-export const worker = setupWorker(...handlers)
+export { worker } from '@pensjonskalkulator-frontend-monorepo/mocks/browser'
