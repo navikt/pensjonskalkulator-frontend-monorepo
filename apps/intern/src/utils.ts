077c99244b4fde4aa731df6b1cfa2f5a99d05179
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2026-04-23 14:15:29 +0200

    Vis omstillingsstønad/gjenlevendeytelse-status i personlinjen

--- apps/intern/src/utils.ts
+++ apps/intern/src/utils.ts
@@ -1,4 +1,7 @@
-import type { LoependeVedtak } from '@pensjonskalkulator-frontend-monorepo/types'
+import type {
+	LoependeVedtak,
+	OmstillingsstoenadOgGjenlevende,
+} from '@pensjonskalkulator-frontend-monorepo/types'
 
 export function getPidFromUrl(): string | undefined {
 	const params = new URLSearchParams(window.location.search)
@@ -6,8 +9,13 @@ export function getPidFromUrl(): string | undefined {
 }
 
 export function getLoependeVedtakStatus(
-	loependeVedtak?: LoependeVedtak
+	loependeVedtak?: LoependeVedtak,
+	omstillingsstoenad?: OmstillingsstoenadOgGjenlevende
 ): string {
+	if (omstillingsstoenad?.harLoependeSak) {
+		return 'Omstillingsstønad/gjenlevendeytelse'
+	}
+
 	if (!loependeVedtak || !loependeVedtak.harLoependeVedtak) {
 		return 'Uten vedtak'
 	}
