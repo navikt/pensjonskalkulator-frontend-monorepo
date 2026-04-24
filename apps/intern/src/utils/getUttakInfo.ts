2483a1820edba7f6b28071bc82fbc0f040553ade
w1n1c0d3s <eshaswini.murali.krishna@nav.no>
2026-04-24 13:11:31 +0200

    Fix afpPrivat maanedsbeloep for 0% uttaksgrad

--- apps/intern/src/utils/getUttakInfo.ts
+++ apps/intern/src/utils/getUttakInfo.ts
@@ -5,39 +5,39 @@ import type { BeregningParams } from '../api/beregningTypes'
 export function getUttakInfo(aktivBeregning: BeregningParams | null) {
 	const erUttaksgradNull =
 		aktivBeregning !== null && aktivBeregning.uttaksgrad === 0
+
 	const erGradert =
 		aktivBeregning !== null &&
 		aktivBeregning.uttaksgrad !== null &&
 		aktivBeregning.uttaksgrad < 100 &&
 		aktivBeregning.uttaksgrad !== 0
 
-	const heltUttakAar =
-		erGradert || erUttaksgradNull
-			? aktivBeregning.alderAarHeltUttak
-			: aktivBeregning?.alderAarUttak
+	const heltUttakAar = erGradert
+		? aktivBeregning.alderAarHeltUttak
+		: aktivBeregning?.alderAarUttak
 
-	const heltUttakMaaned =
-		erGradert || erUttaksgradNull
-			? aktivBeregning.alderMdHeltUttak
-			: aktivBeregning?.alderMdUttak
+	const heltUttakMaaned = erGradert
+		? aktivBeregning.alderMdHeltUttak
+		: aktivBeregning?.alderMdUttak
 
 	const heltUttakAlder: Alder = {
 		aar: heltUttakAar ?? 0,
 		maaneder: heltUttakMaaned ?? 0,
 	}
 
-	const gradertUttakAar = erGradert ? aktivBeregning?.alderAarUttak : undefined
+	const gradertUttakAar =
+		erGradert || erUttaksgradNull ? aktivBeregning?.alderAarUttak : undefined
 
-	const gradertUttakMaaned = erGradert
-		? aktivBeregning?.alderMdUttak
-		: undefined
+	const gradertUttakMaaned =
+		erGradert || erUttaksgradNull ? aktivBeregning?.alderMdUttak : undefined
 
-	const gradertUttakAlder: Alder | undefined = erGradert
-		? {
-				aar: gradertUttakAar ?? 0,
-				maaneder: gradertUttakMaaned ?? 0,
-			}
-		: undefined
+	const gradertUttakAlder: Alder | undefined =
+		erGradert || erUttaksgradNull
+			? {
+					aar: gradertUttakAar ?? 0,
+					maaneder: gradertUttakMaaned ?? 0,
+				}
+			: undefined
 
 	return { erGradert, heltUttakAlder, gradertUttakAlder }
 }
