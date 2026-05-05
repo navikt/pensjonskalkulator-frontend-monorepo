ac4a5a0eb3a01009a10dfaa1a7f954d84c933930
EMK <eshaswini.murali.krishna@nav.no>
2026-05-05 13:10:17 +0200

    Ny vedtak endepunkt (#141)
    * Show EPS vedtak info fetched from v1/vedtak
    
    * Fix vedtak URL
    
    * Show AP for 67 år for helt uttak and do not show opplysninger if EPS is alive
    
    * Add vedtak uten vedtak mock
    
    * Set 0 when utenlandsopphold for EPS is undefined or null
    
    * Validate EPS fields only if EPS is not avdød and does nto have vedtak
    
    * Add VEDTAK to list of API_URLS
    
    * Add check to show AP and AFP table
    
    * Add zebra stripes on opplysninger info, show dynamic grunnbeloep, show AP start dato for avdoed
    
    * Set values from vedtakInfo on eps info when eps is dead
    
    * Remove wrong paranthesis

--- apps/intern/src/components/BeregningForm/useFormValidation.ts
+++ apps/intern/src/components/BeregningForm/useFormValidation.ts
@@ -29,7 +29,6 @@ function validateEPSOpplysninger(
 		errors.epsAntallUtenlandsOppholdAar =
 			'Fyll ut år bodd/jobbet i utlandet etter fylte 16 år.'
 	}
-
 	if (
 		formData.epsAntallUtenlandsOppholdAar !== null &&
 		Number(formData.epsAntallUtenlandsOppholdAar) > 39
@@ -38,9 +37,9 @@ function validateEPSOpplysninger(
 			'Antall år i utlandet kan ikke være større enn 39 år.'
 	}
 
-	if (formData.epsPensjonsgivendeInntektFoerDoedsDato === null) {
-		errors.epsPensjonsgivendeInntektFoerDoedsDato =
-			'Fyll ut inntekt året før dødsdato.'
+	if (formData.epsMedlemAvFolketrygdenVedDoedsDato === null) {
+		errors.epsMedlemAvFolketrygdenVedDoedsDato =
+			'Velg ja/nei om avdøde var medlem av folketrygden.'
 	}
 
 	const epsFoedselsdato =
@@ -59,14 +58,14 @@ function validateEPSOpplysninger(
 			'Velg ja/nei om inntekt ved dødsdato var minst 1G.'
 	}
 
-	if (formData.epsMedlemAvFolketrygdenVedDoedsDato === null) {
-		errors.epsMedlemAvFolketrygdenVedDoedsDato =
-			'Velg ja/nei om avdøde var medlem av folketrygden.'
-	}
-
 	if (formData.epsRegistretSomFlykting === null) {
 		errors.epsRegistretSomFlykting =
 			'Velg ja/nei om avdøde var registrert som flyktning.'
+	}
+
+	if (formData.epsPensjonsgivendeInntektFoerDoedsDato === null) {
+		errors.epsPensjonsgivendeInntektFoerDoedsDato =
+			'Fyll ut inntekt året før dødsdato.'
 	}
 }
 
