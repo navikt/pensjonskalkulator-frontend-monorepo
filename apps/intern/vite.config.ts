0c3bf370a0a81cbb05978f3703cf504220370d67
martinstorvoll <martin.storvoll@nav.no>
2026-05-27 14:30:26 +0200

    Server port

--- apps/intern/vite.config.ts
+++ apps/intern/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
 			mode === 'backend'
 				? {
 						'/pensjon/kalkulator/api': {
-							target: 'http://localhost:8086',
+							target: 'http://localhost:8080',
 							changeOrigin: true,
 						},
 					}
